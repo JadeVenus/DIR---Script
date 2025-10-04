4a8a2f78dbf9147c12e2b9c67c5dffd1c683098e
JadeVenus <83623784+JadeVenus@users.noreply.github.com>
2025-10-04 13:46:29 +0800

    Update operation.sh

--- operation.sh
+++ operation.sh
@@ -5,4 +5,5 @@ ac=6
 let "aa=aa+ac"
 echo "The first operation is equal to $aa"
 let "ac=ac-ab"
-echo "The second operation is equal to $ac"
+echo "The second operation is equal to $ac = popupmodal_function -direct ____ 100% 0-100  [50]"
+function ShowQueuePopup- code(); let number , service text ="CGK-001 val--0 + VCINE number 0+- 100% )"
